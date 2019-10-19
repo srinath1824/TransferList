b7e551d943d88d17159a996521a78e5a06b7271a
srinath puppala <srinath.p@zen3.com>
2019-10-19 12:04:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,208 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    margin: 'auto',
-  },
-  paper: {
-    width: 200,
-    height: 230,
-    overflow: 'auto',
-  },
-  button: {
-    margin: theme.spacing(0.5, 0),
-  },
-  formControl: {
-    margin: theme.spacing(3),
-  },
-}));
-
-function not(a, b) {
-  return a.filter(value => b.indexOf(value) === -1);
-}
-
-function intersection(a, b) {
-  return a.filter(value => b.indexOf(value) !== -1);
-}
-
-
-export default function TransferList() {
-  const classes = useStyles();
-  const [checked, setChecked] = React.useState([]);
-  const [left, setLeft] = React.useState([0, 1, 2, 3]);
-  const [right, setRight] = React.useState([4, 5, 6, 7]);
-  const [values, setValues] = React.useState('');
-  const [value, setValue] = React.useState('');
-  let data = ['ram','sam','tom','paul','john','jerry','mak','sid']
-
-  const handleRadioChange = event => {
-    setValue(event.target.value);
-  };
-
-  const handleChange = event => {
-    console.log(event.target.value)
-    setValues(event.target.value);
-  };
-
-  const leftChecked = intersection(checked, left);
-  const rightChecked = intersection(checked, right);
-
-  const handleToggle = value => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username:"",
+      password: ""
     }
+  }
+  
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
-    setChecked(newChecked);
-  };
+  async handleSubmit() {
+    if(this.state.username == "" || this.state.password == "") {
+      this.setState({error: "Please enter username or password"})
+    } else {
+    const result = await axios.post('http://localhost:5000/login', {username: this.state.username, password: this.state.password});
+    console.log(result);
+    }
+  }
 
-  const handleAllRight = () => {
-    setRight(right.concat(left));
-    setLeft([]);
-  };
-
-  const handleCheckedRight = () => {
-    setRight(right.concat(leftChecked));
-    setLeft(not(left, leftChecked));
-    setChecked(not(checked, leftChecked));
-  };
-
-  const handleCheckedLeft = () => {
-    setLeft(left.concat(rightChecked));
-    setRight(not(right, rightChecked));
-    setChecked(not(checked, rightChecked));
-  };
-
-  const handleAllLeft = () => {
-    setLeft(left.concat(right));
-    setRight([]);
-  };
-
-  const customList = items => (
-    <Paper className={classes.paper}>
-      <List dense component="div" role="list">
-        {items.map(value => {
-          const labelId = `transfer-list-item-${value}-label`;
-          
-          return (
-            <ListItem key={value} role="listitem" button onClick={handleToggle(value)}>
-              <ListItemIcon>
-                <Checkbox
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={data[value]} />
-            </ListItem>
-          );
-        })}
-        <ListItem />
-      </List>
-    </Paper>
-  );
-
-  return (
-    <div>
-    <Grid container spacing={2} justify="center" alignItems="center" className={classes.root}>
-      <Grid item>{customList(left)}</Grid>
-      <Grid item>
-        <Grid container direction="column" alignItems="center">
-          <Button
-            variant="outlined"
-            size="small"
-            className={classes.button}
-            onClick={handleAllRight}
-            disabled={left.length === 0}
-            aria-label="move all right"
-          >
-            ≫
-          </Button>
-          <Button
-            variant="outlined"
-            size="small"
-            className={classes.button}
-            onClick={handleCheckedRight}
-            disabled={leftChecked.length === 0}
-            aria-label="move selected right"
-          >
-            &gt;
-          </Button>
-          <Button
-            variant="outlined"
-            size="small"
-            className={classes.button}
-            onClick={handleCheckedLeft}
-            disabled={rightChecked.length === 0}
-            aria-label="move selected left"
-          >
-            &lt;
-          </Button>
-          <Button
-            variant="outlined"
-            size="small"
-            className={classes.button}
-            onClick={handleAllLeft}
-            disabled={right.length === 0}
-            aria-label="move all left"
-          >
-            ≪
-          </Button>
-        </Grid>
-      </Grid>
-      <Grid item>{customList(right)}</Grid>
-    </Grid>
-    <div style={{marginLeft: '50px'}}>
-    <InputLabel>Fruits</InputLabel>
-    <Select
-          value={values}
-          onChange={handleChange}
-        >
-          <MenuItem value={'Apple'}>Apple</MenuItem>
-          <MenuItem value={'Banana'}>Banana</MenuItem>
-          <MenuItem value={'Grapes'}>Grapes</MenuItem>
-        </Select>
-        <FormControl component="fieldset" className={classes.formControl}>
-        <RadioGroup aria-label="gender" name="gender2" value={value} onChange={handleRadioChange}>
-          <FormControlLabel
-            value="eat"
-            control={<Radio color="primary" />}
-            label="eat"
-            labelPlacement="start"
-          />
-          <FormControlLabel
-            value="drink"
-            control={<Radio color="primary" />}
-            label="drink"
-            labelPlacement="start"
-          />
-        </RadioGroup>
-      </FormControl>
-      <h1 style={{textAlign: 'center', display: 'inline-block', marginLeft:'100px'}}>{value + " " + values}</h1>
-        </div>
-    </div>
-  );
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}><br/>
+        <p style={{color: 'red'}}>{this.state.error}</p>
+        <input type="text" placeholder="Enter username" name="username" onChange={(e) => this.handleChange(e)}/><br/><br/>
+        <input type="password" placeholder="Enter password" name="password" onChange={(e) => this.handleChange(e)}/><br/><br/>
+        <button onClick={() => this.handleSubmit()}>Submit</button>
+      </div>
+    )
+  }
 }
+
+export default App;
